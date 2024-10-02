8041acf902dec93bb99a8d017a551b28797b5987
u16ptr <u16ptr@proton.me>
2024-10-02 08:35:17 +0200

    Creating a simple window

--- src/Main.c
+++ src/Main.c
@@ -1,6 +1,8 @@
 #include <X11/Xlib.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
 
 const char* X_DISPLAY_NAME = ":0";
 
@@ -21,6 +23,28 @@ int main()
 
   printf("Width: %d\n", screenWidth);
   printf("Height: %d\n", screenHeight);
+
+  Window testWindow;
+  int windowX = 16;
+  int windowY = 16;
+  unsigned int windowWidth = 128;
+  unsigned int windowHeight = 128;
+
+  testWindow = XCreateSimpleWindow(
+    display,
+    RootWindow(display, screenNum),
+    windowX,
+    windowY,
+    windowWidth,
+    windowHeight,
+    0,
+    BlackPixel(display, screenNum),
+    WhitePixel(display, screenNum)
+  );
+  XMapWindow(display, testWindow);
+  XSync(display, false);
+
+  sleep(5);
 
   XCloseDisplay(display);
   return EXIT_SUCCESS;
