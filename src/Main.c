288eacd8383b0b789812163a07716ccea6306daf
u16ptr <u16ptr@proton.me>
2024-10-02 13:55:06 +0200

    Setting border and background colors of the panel

--- src/Main.c
+++ src/Main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <unistd.h>
 
 const unsigned int WINDOW_BORDER_WIDTH = 2;
@@ -9,6 +10,8 @@ const unsigned int PANEL_WIDTH = 240;
 const unsigned int PANEL_HEIGHT = 48;
 const unsigned int PANEL_BOTTOM_OFFSET = 0;
 const char* X_DISPLAY_NAME = ":0";
+
+unsigned long calculateRGB(uint8_t red, u_int8_t green, uint8_t blue);
 
 int main()
 {
@@ -28,13 +31,16 @@ int main()
   printf("Width: %d\n", screenWidth);
   printf("Height: %d\n", screenHeight);
 
-  Window testWindow;
+  Window panelWindow;
   int windowX = screenWidth / 2 - PANEL_WIDTH / 2;
   int windowY = screenHeight - PANEL_HEIGHT - PANEL_BOTTOM_OFFSET;
   unsigned int windowWidth = PANEL_WIDTH;
   unsigned int windowHeight = PANEL_HEIGHT;
 
-  testWindow = XCreateSimpleWindow(
+  unsigned long cBackground = calculateRGB(17, 17, 17);
+  unsigned long cBorder = calculateRGB(139, 212, 156);
+
+  panelWindow = XCreateSimpleWindow(
     display,
     RootWindow(display, screenNum),
     windowX,
@@ -42,16 +48,21 @@ int main()
     windowWidth,
     windowHeight,
     WINDOW_BORDER_WIDTH,
-    BlackPixel(display, screenNum),
-    WhitePixel(display, screenNum)
+    cBorder,
+    cBackground
   );
+
+  GC graphicsContext;
+  graphicsContext = XCreateGC(display, panelWindow, 0, 0);
 
   XSetWindowAttributes windowAttributes;
   windowAttributes.override_redirect = true;
-  XChangeWindowAttributes(display, testWindow, CWOverrideRedirect, &windowAttributes);
+  XChangeWindowAttributes(display, panelWindow, CWOverrideRedirect, &windowAttributes);
 
-  XSelectInput(display, testWindow, KeyPressMask);
-  XMapWindow(display, testWindow);
+  XLowerWindow(display, panelWindow);
+  XSelectInput(display, panelWindow, ExposureMask | KeyPressMask);
+  XMapWindow(display, panelWindow);
+  XClearWindow(display, panelWindow);
 
   XEvent event;
   while (true)
@@ -59,6 +70,12 @@ int main()
     XNextEvent(display, &event);
   }
 
+  XFreeGC(display, graphicsContext);
   XCloseDisplay(display);
   return EXIT_SUCCESS;
+}
+
+unsigned long calculateRGB(uint8_t red, u_int8_t green, uint8_t blue)
+{
+  return blue + (green << 8) + (red << 16);
 }
