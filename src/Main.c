c1dae73df8edd36071b77d835e65d81d15391156
u16ptr <u16ptr@proton.me>
2024-10-02 08:26:05 +0200

    Connecting to the X11 server

--- src/Main.c
+++ src/Main.c
@@ -1,8 +1,27 @@
+#include <X11/Xlib.h>
 #include <stdlib.h>
 #include <stdio.h>
 
+const char* X_DISPLAY_NAME = ":0";
+
 int main()
 {
-  printf("Hello, world!\n");
+  Display* display;
+  display = XOpenDisplay(X_DISPLAY_NAME);
+
+  if (display == NULL)
+  {
+    fprintf(stderr, "Cannot connect to X server: %s!\n", X_DISPLAY_NAME);
+    return EXIT_FAILURE;
+  }
+
+  int screenNum = DefaultScreen(display);
+  int screenWidth = DisplayWidth(display, screenNum);
+  int screenHeight = DisplayHeight(display, screenNum);
+
+  printf("Width: %d\n", screenWidth);
+  printf("Height: %d\n", screenHeight);
+
+  XCloseDisplay(display);
   return EXIT_SUCCESS;
 }
